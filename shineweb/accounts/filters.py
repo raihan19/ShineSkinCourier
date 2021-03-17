104044dff2827bb43ccfeba88b5a6868af5f0533
raihan19 <raihanul.haque@northsouth.edu>
2021-03-17 12:35:54 +0600

    Added label to django filter, changed html code for data representation

--- shineweb/accounts/filters.py
+++ shineweb/accounts/filters.py
@@ -4,10 +4,10 @@ from django_filters import DateFilter, CharFilter
 
 
 class OrderFilter(django_filters.FilterSet):
-	start_date = DateFilter(field_name="date_created", lookup_expr='gte')
-	end_date = DateFilter(field_name="date_created", lookup_expr='lte')
-	note = CharFilter(field_name='note', lookup_expr='icontains')
+	start_date = DateFilter(field_name="date_created", lookup_expr='gte', label='Start date')
+	end_date = DateFilter(field_name="date_created", lookup_expr='lte', label='End date')
+	note = CharFilter(field_name='note', lookup_expr='icontains', label='Note')
 
 	class Meta:
 		model = Order
-		fields = ['customer_name', 'product_name', 'status']
+		fields = ['merchant__username', 'status']
