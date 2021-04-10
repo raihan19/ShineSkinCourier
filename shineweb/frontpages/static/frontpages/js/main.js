4714a37fc6f2655bb90ac575810ce2d50e844ee7
raihan19 <raihanul.haque@northsouth.edu>
2021-04-11 01:03:55 +0600

    Changed front page and about page

--- shineweb/frontpages/static/frontpages/js/main.js
+++ shineweb/frontpages/static/frontpages/js/main.js
@@ -1,286 +1,141 @@
-jQuery(document).ready(function ($) {
-
-
-    $(window).bind('resizeEnd', function () {
-        $('.header_area,.singlebgslider').css('height', $(window).height());
-    });
-
-    $(window).resize(function () {
-
-        if (this.resizeTo) clearTimeout(this.resizeTo);
-        setTimeout(function () {
-            $(this).trigger('resizeEnd');
-        });
-
-    }).trigger('resize')
-
-    $("#sticker").sticky({
-        topSpacing: 0,
-        zIndex: 5555
-    });
+(function ($) {
+    "use strict";
     
-    
-    $('.navbar').meanmenu();
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-    $('.single_calculate').click(function () {
-        $(this).addClass("addcolor").siblings('div.single_calculate').removeClass('addcolor');
-    });
-
-    $(function () {
-        $(".player").YTPlayer();
-    });
-
-    $('.hero-slider').owlCarousel({
-        items: 1,
-        loop: true,
-        themeClass: 'hero-theme',
-        nav: true,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-    });
-
-    $('.active-bg-slider').owlCarousel({
-        items: 1,
-        loop: true,
-        themeClass: 'hero-theme',
-        autoplay: true,
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-    });
-
-    $('.active-hero4-slider').owlCarousel({
-        items: 1,
-        themeClass: 'hero4-theme',
-        autoplay: true,
-        loop: true,
-
-    });
-
-
-    //Pricing Table Slider
-    //slideshow style interval
-    var autoSwap = setInterval(swap, 3500);
-
-    //pause slideshow and reinstantiate on mouseout
-    $('ul, span').hover(
-        function () {
-            clearInterval(autoSwap);
-        },
-        function () {
-            // autoSwap = setInterval(swap, 5000);
-        });
-
-    //global variables
-    var items = [];
-    var startItem = 1;
-    var position = 0;
-    var itemCount = $('.carousel li.items').length;
-    var leftpos = itemCount;
-    var resetCount = itemCount;
-
-    //unused: gather text inside items class
-    $('li.items').each(function (index) {
-        items[index] = $(this).text();
-    });
-
-    //swap images function
-    function swap(action) {
-        var direction = action;
-
-        //moving carousel backwards
-        if (direction == 'counter-clockwise') {
-            var leftitem = $('.left-pos').attr('id') - 1;
-            if (leftitem == 0) {
-                leftitem = itemCount;
-            }
-
-            $('.right-pos').removeClass('right-pos').addClass('back-pos');
-            $('.main-pos').removeClass('main-pos').addClass('right-pos');
-            $('.left-pos').removeClass('left-pos').addClass('main-pos');
-            $('#' + leftitem + '').removeClass('back-pos').addClass('left-pos');
-
-            startItem--;
-            if (startItem < 1) {
-                startItem = itemCount;
-            }
-        }
-
-        //moving carousel forward
-        if (direction == 'clockwise' || direction == '' || direction == null) {
-            function pos(positionvalue) {
-                if (positionvalue != 'leftposition') {
-                    //increment image list id
-                    position++;
-
-                    //if final result is greater than image count, reset position.
-                    if ((startItem + position) > resetCount) {
-                        position = 1 - startItem;
-                    }
-                }
-
-                //setting the left positioned item
-                if (positionvalue == 'leftposition') {
-                    //left positioned image should always be one left than main positioned image.
-                    position = startItem - 1;
-
-                    //reset last image in list to left position if first image is in main position
-                    if (position < 1) {
-                        position = itemCount;
-                    }
-                }
-
-                return position;
-            }
-
-            $('#' + startItem + '').removeClass('main-pos').addClass('left-pos');
-            $('#' + (startItem + pos()) + '').removeClass('right-pos').addClass('main-pos');
-            $('#' + (startItem + pos()) + '').removeClass('back-pos').addClass('right-pos');
-            $('#' + pos('leftposition') + '').removeClass('left-pos').addClass('back-pos');
-
-            startItem++;
-            position = 0;
-            if (startItem > itemCount) {
-                startItem = 1;
-            }
-        }
-    }
-
-    //next button click function
-    $('#next').click(function () {
-        swap('clockwise');
-    });
-
-    //prev button click function
-    $('#prev').click(function () {
-        swap('counter-clockwise');
-    });
-
-    //if any visible items are clicked
-    $('li').click(function () {
-        if ($(this).attr('class') == 'items left-pos') {
-            swap('counter-clockwise');
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
         } else {
-            swap('clockwise');
+            $('.back-to-top').fadeOut('slow');
         }
     });
-
-
-    $(".Modern-Slider").slick({
-        autoplay: true,
-        autoplaySpeed: 10000,
-        speed: 900,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        pauseOnHover: false,
-        dots: true,
-        pauseOnDotsHover: true,
-        cssEase: 'linear',
-        fade: true, // Disable This And Watch
-        draggable: false,
-        prevArrow: '<button class="PrevArrow"></button>',
-        nextArrow: '<button class="NextArrow"></button>',
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
     });
     
-    $(window).load(function () {
-        $('#preloader').fadeOut('slow', function () {
-            $(this).remove();
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+
+    
+    // Main carousel
+    $(".carousel .owl-carousel").owlCarousel({
+        autoplay: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items: 1,
+        smartSpeed: 300,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ]
+    });
+    
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
         });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+    
+    // jQuery counterUp
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
     });
 
 
-    if (window.google) {
-
-        function initialize() {
-            var mapProp = {
-                center: new google.maps.LatLng(51.508742, -0.120850),
-                zoom: 14,
-                scrollwheel: false,
-                styles: [{
-                    "featureType": "landscape",
-                    "stylers": [{
-                        "saturation": -100
-                    }, {
-                        "lightness": 65
-                    }, {
-                        "visibility": "on"
-                    }]
-                }, {
-                    "featureType": "poi",
-                    "stylers": [{
-                        "saturation": -100
-                    }, {
-                        "lightness": 51
-                    }, {
-                        "visibility": "simplified"
-                    }]
-                }, {
-                    "featureType": "road.highway",
-                    "stylers": [{
-                        "saturation": -100
-                    }, {
-                        "visibility": "simplified"
-                    }]
-                }, {
-                    "featureType": "road.arterial",
-                    "stylers": [{
-                        "saturation": -100
-                    }, {
-                        "lightness": 30
-                    }, {
-                        "visibility": "on"
-                    }]
-                }, {
-                    "featureType": "road.local",
-                    "stylers": [{
-                        "saturation": -100
-                    }, {
-                        "lightness": 40
-                    }, {
-                        "visibility": "on"
-                    }]
-                }, {
-                    "featureType": "transit",
-                    "stylers": [{
-                        "saturation": -100
-                    }, {
-                        "visibility": "simplified"
-                    }]
-                }, {
-                    "featureType": "administrative.province",
-                    "stylers": [{
-                        "visibility": "off"
-                    }]
-                }, {
-                    "featureType": "water",
-                    "elementType": "labels",
-                    "stylers": [{
-                        "visibility": "on"
-                    }, {
-                        "lightness": -25
-                    }, {
-                        "saturation": -100
-                    }]
-                }, {
-                    "featureType": "water",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "hue": "#ffff00"
-                    }, {
-                        "lightness": -25
-                    }, {
-                        "saturation": -97
-                    }]
-                }],
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        animateIn: 'slideInDown',
+        animateOut: 'slideOutDown',
+        items: 1,
+        smartSpeed: 450,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
         }
-        google.maps.event.addDomListener(window, 'load', initialize);
+    });
+    
+    
+    // Blogs carousel
+    $(".blog-carousel").owlCarousel({
+        autoplay: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
 
-
-    }
-
-});
